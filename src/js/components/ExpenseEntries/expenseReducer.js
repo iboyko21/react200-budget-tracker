050b79fb47b080199e7ff1da671070f78d30b3a2
Igor Boyko <iboyko@gmail.com>
2022-12-22 17:02:38 -0800

    updated actions and added connect()

--- src/js/components/ExpenseEntries/expenseReducer.js
+++ src/js/components/ExpenseEntries/expenseReducer.js
@@ -7,5 +7,51 @@ const defaultState = {
 
 export default function ExpenseReducer (state = defaultState, action) {
 // the 'state = defaultState' above is new ES6 syntax for defining a default value on a parameter
+    const { type, payload } = action;
+
+    switch (type) {
+        // Here in the case of the update description action
+        case  'UPDATE_EXPENSE_DESCRIPTION': {
+            // we'll return an object
+            return {
+                // with all the previous state
+                ...state,
+                // but overwritting the description
+                description: payload.description
+            };
+        }
+        case 'UPDATE_EXPENSE_AMOUNT': {
+            return {
+                ...state,
+                amount: payload.amount
+            };
+        }
+        case 'ADD_EXPENSE': {
+            const { description, amount } = action.payload;
+            return {
+                description: '',
+                action: '',
+                lineItems: [
+                    // here we have all the previous line items
+                    ...state.lineItems,
+                    // plus a new object
+                    { description, amount }
+                ]
+            };
+        }
+
+        default: {
+            return state;
+        }
+    }
+
+
+
+
+
+
+
+
+
     return state;
 }
