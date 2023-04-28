ddc067395a98fac694266ef0dfe987c9f536894b
Igor Boyko <iboyko@gmail.com>
2023-04-28 15:36:49 -0700

    removed some comments

--- src/js/components/ExpenseEntries/expenseReducer.js
+++ src/js/components/ExpenseEntries/expenseReducer.js
@@ -4,7 +4,6 @@ const defaultState = {
     lineItems: []
 };
 
-
 export default function ExpenseReducer (state = defaultState, action) {
 // the 'state = defaultState' above is new ES6 syntax for defining a default value on a parameter
     const { type, payload } = action;
--- src/js/components/ExpenseEntries/index.js
+++ src/js/components/ExpenseEntries/index.js
@@ -1,7 +1,6 @@
 import { connect } from "react-redux";
 import ExpenseEntries from "./ExpenseEntries";
 
-
 // This function takes the store and returns an object
 // that's passed to the props of the component.
 function mapStoreToProps(store) {
@@ -11,7 +10,6 @@ function mapStoreToProps(store) {
         lineItems: store.expense.lineItems
     };
 }
-
 
 // This might look odd, but connect returns a function,
 // that is then called with the componenst itself.
